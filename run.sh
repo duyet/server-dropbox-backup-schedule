13e64618314fa45d92294f5ea20dff64d4684600
Van-Duyet Le <lvduit08@gmail.com>
2016-03-12 13:33:55 -0500

    backup src

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
+SOURCE_FOLDER="/root/project/quick"
+UPLOAD_FOLDER="./upload"
+
+######################
+
 function print
 {
 	echo "$1";
@@ -12,7 +17,8 @@ print "============================="
 
 print ""
 print ">> Copy source ..."
-
+print "Copy $SOURCE_FOLDER to $UPLOAD_FOLDER..."
+cp "$SOURCE_FOLDER/*" "$UPLOAD_FOLDER/src" -R
 print "OK"
 
 print ""
